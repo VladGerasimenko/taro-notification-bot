320831a373274d812c56ddc7cfc457fbe1512df9
Parashnik <rybachok@gmail.com>
2023-12-15 20:22:45 +0700

    Shadow token

--- src/main/java/org/example/taro_notification_bot/TaroNotificationBotStartup.java
+++ src/main/java/org/example/taro_notification_bot/TaroNotificationBotStartup.java
@@ -1,17 +1,27 @@
 package org.example.taro_notification_bot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.Properties;
+
+@Slf4j
 public class TaroNotificationBotStartup {
 
-    public static void main(String[] args) {
-        ConfigReader configReader = new ConfigReader();
-        TelegramBot telegramBot = new TelegramBot(configReader.get());
-
-        try {
-            telegramBot.initApi();
-        } catch (TelegramApiException e) {
-            System.exit(1);
+    public static void main(String[] args) throws TelegramApiException{
+        if (args.length != 1) {
+            printUsage();
+        } else {
+            ConfigReader configReader = new ConfigReader();
+            Properties properties = configReader.get();
+            properties.put("bot.token", args[0]);
+            new TelegramBot(properties).initApi();
         }
+    }
+
+    private static void printUsage() {
+        log.info("-------------------Usage--------------------");
+        log.info("-------------Command line args -------------");
+        log.info("-------------(1) bot token -----------------");
     }
 }
